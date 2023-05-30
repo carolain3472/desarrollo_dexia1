2ed6045e01dcf75ded8cfee86335c7c04543497a
nandolopez7 <hernando.rincon@correounivalle.edu.co>
2023-05-29 22:28:38 -0500

    Se corrije el diseño de navbar para que esté fijo, se agrega logo Dexia y se añade opcion de visualizar cual item está seleccionado, se corrige distribucion de pagina inicio y registro

--- frontend/src/pages/inicio_app.jsx
+++ frontend/src/pages/inicio_app.jsx
@@ -1,33 +1,26 @@
-import { NavbarAdmin } from '../components/Nav_bar'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faAnchor, faHome, faTachometerAlt, faFileUpload, faPhotoVideo, faBuildingColumns, faUsers, faMoneyCheckAlt, faCogs, faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import '@fortawesome/fontawesome-svg-core/styles.css';
-import React from 'react'
+import { NavbarAdmin } from "../components/Nav_bar";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import React from "react";
 
 export function Inicio_app() {
-    const storedNombre= sessionStorage.getItem("nombre")
+  const storedNombre = sessionStorage.getItem("nombre");
   return (
     <div>
-
-<NavbarAdmin/>
-
-
-          <div className='dashboard-app' style={{ marginTop: 0, paddingTop: 0 }} >
-
-          <div className='dashboard-content'>
-            <div className='container'>
-              <div className='card'>
-                <div className='card-header' >
-                  <h1>Bienvenid@ {storedNombre}</h1>
-                </div>
-                <div className='card-body'>
-                  <p>Estás dentro del sistema como: Administrador</p>
-                </div>
+      <NavbarAdmin />
+      <div style={{ marginLeft: "250px", marginTop:"10px", marginRight: "10px"}}>
+        <div>
+          <div className="container">
+            <div className="card">
+              <div className="card-header">
+                <h1>Bienvenid@ {storedNombre}</h1>
+              </div>
+              <div className="card-body">
+                <p>Estás dentro del sistema como: Administrador</p>
               </div>
             </div>
           </div>
-  </div> 
-
+        </div>
+      </div>
     </div>
-  )
+  );
 }
