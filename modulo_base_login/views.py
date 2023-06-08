a499813f39b3ca6b734f1ef99e5c6e5b2d9bec9e
SantiagoCasanas <santiago.casanas@correounivalle.edu.co>
2023-06-08 17:06:31 -0500

    import q

--- modulo_base_login/views.py
+++ modulo_base_login/views.py
@@ -18,6 +18,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.permissions import AllowAny
+from django.db.models import Q
 
 from .serializer import UsuarioSerializer
 from rest_framework import viewsets
