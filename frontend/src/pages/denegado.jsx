fc92854a4668e55c8440278fe664033a97d1c9b3
nandolopez7 <hernando.rincon@correounivalle.edu.co>
2023-06-16 01:20:10 -0500

    Se crea interfaz de pagina acceso denegado, se incluye boton de redireccionamiento a inicio de sesion

--- frontend/src/pages/denegado.jsx
+++ frontend/src/pages/denegado.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from "react";
+import "../Scss/denegado.css";
+import { Link } from "react-router-dom";
 
 export function Acceso_denegado() {
   return (
-    <div>No tiene permisos suficientes para acceder a esta Página</div>
-  )
-}
+    <>
+      <nav className="nav-bar-denegado">
+        <a><img src="/images/univalle-logo-white.png" alt=""/>Universidad del Valle</a>
+      </nav>
 
+      <div className="notFound-container">
+        <img src="/images/noEncontrado.jpg" alt="" />
+        <div className="notFound-text">
+          <h1>Acceso denegado</h1>
+          <h6>Verifica tus credenciales</h6>
+        </div>
+
+        <Link to="/login" className="boton-inicio">
+          Iniciar sesión
+        </Link>
+      </div>
+    </>
+  );
+}
