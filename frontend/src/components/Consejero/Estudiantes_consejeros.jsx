511b7a9f88777360041c0c13f586a7e651654816
SantiagoCasanas <santiago01.02.003@gmail.com>
2023-06-20 11:50:47 -0500

    filtros en el front realizados, falta diseno y el env para la bd

--- frontend/src/components/Consejero/Estudiantes_consejeros.jsx
+++ frontend/src/components/Consejero/Estudiantes_consejeros.jsx
@@ -1,34 +1,160 @@
-/* import React from 'react'
-
-export function Estudiantes_consejeros() {
-  return (
-    <div>Aqui ira la tabla con la info de los estudiantes</div>
-  )
-} */
-
-
-
 import React, { useState, useEffect } from "react";
 import { api } from "../../api/register_api";
 
 export function Estudiantes_consejeros() {
   const [estudiantes, setEstudiantes] = useState([]);
+  const [identificacionFiltro, setIdentificacionFiltro] = useState("");
+  const [nombreFiltro, setNombreFiltro] = useState("");
+  const [primerApellidoFiltro, setPrimerApellidoFiltro] = useState("");
+  const [correoFiltro, setCorreoFiltro] = useState("");
+  const [codigoFiltro, setCodigoFiltro] = useState("");
+
+
+
+  const handleIdentificacionFilter = (event) => {
+    const identificacion = event.target.value;
+    setIdentificacionFiltro(identificacion);
+    fetchEstudiantes(
+      identificacion,
+      nombreFiltro,
+      primerApellidoFiltro,
+      correoFiltro,
+      codigoFiltro
+    );
+  };
+
+  const handleNombreFilter = (event) => {
+    const nombre = event.target.value;
+    setNombreFiltro(nombre);
+    fetchEstudiantes(
+      identificacionFiltro,
+      nombre,
+      primerApellidoFiltro,
+      correoFiltro,
+      codigoFiltro
+    );
+  };
+
+  const handlePrimerApellidoFilter = (event) => {
+    const primerApellido = event.target.value;
+    setPrimerApellidoFiltro(primerApellido);
+    fetchEstudiantes(
+      identificacionFiltro,
+      nombreFiltro,
+      primerApellido,
+      correoFiltro,
+      codigoFiltro
+    );
+  };
+
+  const handCorreoFilter = (event) => {
+    const correo = event.target.value;
+    setCorreoFiltro(correo);
+    fetchEstudiantes(
+      identificacionFiltro,
+      nombreFiltro,
+      primerApellidoFiltro,
+      correo,
+      codigoFiltro
+    );
+  };
+
+const handCodigoFilter = (event) => {
+    const codigo = event.target.value;
+    setCodigoFiltro(codigo);
+    fetchEstudiantes(
+      identificacionFiltro,
+      nombreFiltro,
+      primerApellidoFiltro,
+      correoFiltro,
+      codigo
+    );
+  };
+
 
   useEffect(() => {
     fetchEstudiantes();
   }, []);
 
-  const fetchEstudiantes = async () => {
-    try {
-      const response = await api.get("/estudiantes/listar_estudiantes");
-      setEstudiantes(response.data);
-    } catch (error) {
-      console.error(error);
-    }
+
+  const fetchEstudiantes = (doc_identidad, nombre, primer_apellido, correo_institucional, codigo_estudiantil) => {
+    const params = new URLSearchParams({
+      doc_identidad,
+      nombre,
+      primer_apellido,
+      correo_institucional,
+      codigo_estudiantil,
+    });
+  
+    const url = `/estudiantes/listar_estudiantes/?${params.toString()}`;
+  
+    api
+      .get(url)
+      .then((response) => {
+        setEstudiantes(response.data);
+        console.log(response.data);
+        console.log(url);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
+
 
    return (
     <div>
+          <div style={{ display: "flex", justifyContent: "space-between", marginLeft: "-300px", marginRight: "50px" }}>
+            <input
+              type="text"
+              placeholder="Filtrar por cédula"
+              value={identificacionFiltro}
+              onChange={handleIdentificacionFilter}
+              style={{ 
+                width: "200px",
+                marginBottom:"15px"}}
+            />
+
+            <input
+              type="text"
+              placeholder="Filtrar por nombre"
+              value={nombreFiltro}
+              onChange={handleNombreFilter}
+              style={{ 
+                width: "200px",
+                marginBottom:"15px" }}
+            />
+
+            <input
+              type="text"
+              placeholder="Filtrar por apellido"
+              value={primerApellidoFiltro}
+              onChange={handlePrimerApellidoFilter}
+              style={{ 
+                width: "200px",
+                marginBottom:"15px" }}
+            />
+
+            <input
+              type="text"
+              placeholder="Filtrar por correo"
+              value={correoFiltro}
+              onChange={handCorreoFilter}
+              style={{ 
+                width: "250px",
+                marginBottom:"15px" }}
+            />
+
+            <input
+              type="text"
+              placeholder="Filtrar por código"
+              value={codigoFiltro}
+              onChange={handCodigoFilter}
+              style={{ 
+                width: "200px",
+                marginBottom:"15px" }}
+            />
+    </div>
+
         <section
           className="intro"
           style={{ marginLeft: "-350px", marginTop: "0px" }}>
