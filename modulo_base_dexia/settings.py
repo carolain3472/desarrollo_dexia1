1d9284b3f0201732d8b488ba3fda74a88c0c5a83
SantiagoCasanas <santiago.casanas@correounivalle.edu.co>
2023-06-21 15:54:28 -0500

    Explicacion para configurar las variables de ambiente para la bd

--- modulo_base_dexia/settings.py
+++ modulo_base_dexia/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -87,11 +92,11 @@ WSGI_APPLICATION = 'modulo_base_dexia.wsgi.application'
 DATABASES = {
    'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dexia',
-        'USER' : 'postgres',
-        'PASSWORD' : '123456',
-        'HOST' : 'localhost',
-        'DATABASE_PORT' : '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_CONTAINER_NAME', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
 }
 }
 
